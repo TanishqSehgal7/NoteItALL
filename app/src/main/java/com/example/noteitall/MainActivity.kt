280ba4f5013eb3b67407f7c3cbbfb584b5f7152f
TanishqSehgal7 <tsgl7246@gmail.com>
2021-03-18 02:15:40 +0530

    Initial commit

---
+++ app/src/main/java/com/example/noteitall/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.noteitall
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE)
+        supportActionBar?.hide()
+        WindowManager.LayoutParams.FLAG_FULLSCREEN
+
+        setContentView(R.layout.activity_main)
+
+        val addNote:FloatingActionButton= findViewById(R.id.Fab_AddNote)
+        addNote.setOnClickListener {
+            val intent=Intent(this,NotesActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
---
+++ app/src/main/java/com/example/noteitall/NotesActivity.kt
@@ -0,0 +1,23 @@
+package com.example.noteitall
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager
+import android.widget.ImageButton
+
+class NotesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE)
+        supportActionBar?.hide()
+        WindowManager.LayoutParams.FLAG_FULLSCREEN
+
+
+        setContentView(R.layout.activity_notes)
+        val backButton:ImageButton=findViewById(R.id.NotActivityExit)
+        backButton.setOnClickListener {
+            finish()
+        }
+    }
+}
